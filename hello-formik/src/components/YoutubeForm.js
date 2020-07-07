3559ee30ef8414e5dd48b74d9dfc889a6655d36e
Taras Hrynchuk <thrynchuk@vertafore.com>
2020-07-07 20:19:52 +0300

    Formik components

--- hello-formik/src/components/YoutubeForm.js
+++ hello-formik/src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useFormik} from "formik";
+import {Formik, Form, Field, ErrorMessage} from "formik";
 import * as Yup from 'yup';
 
 const validationSchema = Yup.object({
@@ -7,53 +7,35 @@ const validationSchema = Yup.object({
     email: Yup.string().email("Invalid email").required('Required'),
     channel: Yup.string().required('Required')
 });
-
-
-const YoutubeForm = () => {
-    const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            channel: ''
-        },
-        onSubmit: values => {
-            console.log('Submitted form', values)
-        },
-        validationSchema
-    });
-    // console.log(formik.values);
-    console.log('visited fields', formik.touched);
-
-    return (
-        <div>
-            <form onSubmit={formik.handleSubmit}>
-                <label htmlFor="name">Name</label>
-                <input type="text" id='name' name='name'
-                       {...formik.getFieldProps('name')}
-                />
-                {formik.touched.name && formik.errors.name ? formik.errors.name: ''}
-
-                <label htmlFor="email">Email</label>
-                <input type="text" id='email' name='email'
-                    {...formik.getFieldProps('email')}
-                />
-                {formik.touched.email && formik.errors.email ? formik.errors.email: ''}
-
-                <label htmlFor="channel">Channel</label>
-                <input type="text" id='channel' name='channel'
-                       {...formik.getFieldProps('channel')}
-                />
-                {formik.touched.channel && formik.errors.channel ? formik.errors.channel: ''}
-
-                <button type='Submit'>Submit</button>
-            </form>
-        </div>
-    );
+const initialValues = {
+    name: '',
+    email: '',
+    channel: ''
+};
+const onSubmit = values => {
+    console.log('Submitted form', values)
 };
 
-function validateEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
+const YoutubeForm = () => {
 
+    return (
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+            <Form>
+                <label htmlFor="name">Name</label>
+                <Field type="text" id='name' name='name'/>
+                <ErrorMessage name='name'/>
+
+                <label htmlFor="email">Email</label>
+                <Field type="text" id='email' name='email'/>
+                <ErrorMessage name='email'/>
+
+                <label htmlFor="chazxlnnel">Channel</label>
+                <Field type="text" id='channel' name='channel'/>
+                <ErrorMessage name='channel'/>
+
+                <button type='Submit'>Submit</button>
+            </Form>
+        </Formik>
+    );
+};
 export default YoutubeForm;
