3dc5b2362a0beb8884db016fe6f7be133e5840e9
Taras Hrynchuk <thrynchuk@vertafore.com>
2020-07-07 15:20:27 +0300

    base form

--- hello-formik/src/App.js
+++ hello-formik/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import YoutubeForm from "./components/YoutubeForm";
 
 function App() {
     return (
         <>
             Hi
+            <YoutubeForm/>
         </>
     );
 }
