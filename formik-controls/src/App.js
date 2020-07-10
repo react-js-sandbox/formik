91dd91bc5c21f0190f14d8160836d906495bc3a4
Taras Hrynchuk <thrynchuk@vertafore.com>
2020-07-11 01:23:49 +0300

    add formik container

--- formik-controls/src/App.js
+++ formik-controls/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import FormikContainer from "./components/FormikContainer";
 
 function App() {
     return (
-        <>
-            Hi
-        </>
+        <div className='App'>
+            <FormikContainer/>
+        </div>
     );
 }
 
---
+++ formik-controls/src/components/FormikContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Formik, Form} from "formik";
+import * as Yup from 'yup';
+
+const FormikContainer = () => {
+    const initialValues = {};
+    const validationSchema = Yup.object({});
+    const onSubmit = values => console.log('Form data', values);
+
+    return (
+        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+            {
+                formik => <Form>
+                    <submit type='submit'>Submit</submit>
+                </Form>
+            }
+        </Formik>
+    );
+};
+
+export default FormikContainer;
