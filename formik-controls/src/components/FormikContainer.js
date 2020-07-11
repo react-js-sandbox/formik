f831e463e51d3e139b6cbb020521441be3472857
Taras Hrynchuk <thrynchuk@vertafore.com>
2020-07-11 15:03:48 +0300

    reusable formik radio

--- formik-controls/src/components/FormikContainer.js
+++ formik-controls/src/components/FormikContainer.js
@@ -10,15 +10,22 @@ const FormikContainer = () => {
         {key: 'Option 2', value: 'option2'},
         {key: 'Option 3', value: 'option3'},
     ];
+    const radioOption = [
+        {key: 'Option 1', value: 'rOption1'},
+        {key: 'Option 2', value: 'rOption2'},
+        {key: 'Option 3', value: 'rOption3'},
+    ];
     const initialValues = {
         email: '',
         description: '',
-        selectOption: ''
+        selectOption: '',
+        radioOption: ''
     };
     const validationSchema = Yup.object({
         email: Yup.string().required('Required'),
         description: Yup.string().required('Required'),
         selectOption: Yup.string().required('Required'),
+        radioOption: Yup.string().required('Required'),
     });
     const onSubmit = values => console.log('Form data', values);
 
@@ -29,6 +36,7 @@ const FormikContainer = () => {
                     <FormikControl control='input' label='Email' name='email'/>
                     <FormikControl control='textarea' label='Description' name='description'/>
                     <FormikControl control='select' label='Select a topic' name='selectOption' options={dropdownOption}/>
+                    <FormikControl control='radio' label='Radio topic' name='radioOption' options={radioOption}/>
                     <button type='submit'>Submit</button>
                 </Form>
             }
