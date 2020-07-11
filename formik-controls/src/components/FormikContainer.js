c1b8a75aaf96ca5de67552705d50eb8d5829770b
Taras Hrynchuk <thrynchuk@vertafore.com>
2020-07-11 15:15:22 +0300

    reusable formik checkboxes

--- formik-controls/src/components/FormikContainer.js
+++ formik-controls/src/components/FormikContainer.js
@@ -15,17 +15,25 @@ const FormikContainer = () => {
         {key: 'Option 2', value: 'rOption2'},
         {key: 'Option 3', value: 'rOption3'},
     ];
+    const checkboxOption = [
+        {key: 'Option 1', value: 'cOption1'},
+        {key: 'Option 2', value: 'cOption2'},
+        {key: 'Option 3', value: 'cOption3'},
+    ];
+
     const initialValues = {
         email: '',
         description: '',
         selectOption: '',
-        radioOption: ''
+        radioOption: '',
+        checkboxOption: []
     };
     const validationSchema = Yup.object({
         email: Yup.string().required('Required'),
         description: Yup.string().required('Required'),
         selectOption: Yup.string().required('Required'),
         radioOption: Yup.string().required('Required'),
+        checkboxOption: Yup.array().required('Required'),
     });
     const onSubmit = values => console.log('Form data', values);
 
@@ -37,6 +45,7 @@ const FormikContainer = () => {
                     <FormikControl control='textarea' label='Description' name='description'/>
                     <FormikControl control='select' label='Select a topic' name='selectOption' options={dropdownOption}/>
                     <FormikControl control='radio' label='Radio topic' name='radioOption' options={radioOption}/>
+                    <FormikControl control='checkbox' label='Check topic' name='checkboxOption' options={checkboxOption}/>
                     <button type='submit'>Submit</button>
                 </Form>
             }
